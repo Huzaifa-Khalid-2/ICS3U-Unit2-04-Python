29f8d0a369acf30b446572628e8265d04af87460
Huzaifa Khalid <huzaifa.khalid@mths.ca>
2022-03-07 19:47:15 +0000

    finally here

---
+++ constants.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# Created by: Huzaifa Khalid
+# Created on: March 2022
+# This module contains the constants for the pizza assignment
+
+
+LABOR = 0.75
+RENT = 1.00
+COST_PER_INCH = 0.50
+HST = 0.13
